b7dda389ac48ffb2cefca4bb8f65a8f4b8e47e47
Christmas-Coin <44874328+Christmas-Coin@users.noreply.github.com>
2019-02-19 20:05:44 +0100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 81790; //release
+static const int PROTOCOL_VERSION = 81791; //release
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -22,7 +22,7 @@ static const int GETHEADERS_VERSION = 70000;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 81790;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 81790;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 81791;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
